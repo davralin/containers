d223adc456e6f39f67633d153cd1c0b4c6571f73
davralin <gpg@davralin.work>
2024-11-13 19:39:08 +0100

    Update ssh-entrypoint

--- sshd/entrypoint.sh
+++ sshd/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ssh-keygen -t ed25519 -f /home/user/ssh_host_ed25519_key -q -N ""
+mkdir -p /home/user/.ssh
+echo $PUBLIC_KEY > /home/user/.ssh/authorized_keys
 /usr/sbin/sshd -D -p 2222
