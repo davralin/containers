611c9df4045fdcd6062d802705985888263f0dbd
davralin <gpg@davralin.work>
2024-11-14 08:19:07 +0100

    Redo ssh-container

--- sshd/entrypoint.sh
+++ sshd/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ssh-keygen -t ed25519 -f /home/user/ssh_host_ed25519_key -q -N ""
-mkdir -p /home/user/.ssh
-echo $PUBLIC_KEY > /home/user/.ssh/authorized_keys
-/usr/sbin/sshd -D -p 2222
+if [ ! -f "/home/user/ssh/ssh_host_ed25519_key" ]; then
+    ssh-keygen -t ed25519 -f /home/user/ssh/ssh_host_ed25519_key -q -N ""
+fi
+echo $PUBLIC_KEY > /home/user/ssh/authorized_keys
+/usr/sbin/sshd -D -e -p 2222
